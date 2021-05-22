f4e3aca420db45fbf66f41e9614b2f5f2001a3d2
ABojo <alex.bojorquez2598@gmail.com>
2021-05-22 17:30:53 -0400

    Pulled buildPosts and getPosts functions out of the App component function to make the code more readable + passed in the state setter function as a callback so it can be called once the async function retrives the data from the server

--- src/App.js
+++ src/App.js
@@ -3,32 +3,33 @@ import Navbar from './Navbar';
 import Post from './Post';
 import Loader from './Loader';
 
+const buildPosts = (posts) => {
+  return JSON.parse(posts).map((post) => {
+    const { body } = post;
+    return (
+      <Post
+        id={post._id}
+        title={post.title}
+        body={`${body.length > 200 ? body.slice(0, 200) : body}...`}
+        author={post.author}
+        stamp={post.timestamp}
+      />
+    );
+  });
+};
+
+const getPosts = async (stateSetter) => {
+  const raw = await fetch('https://odingblogapi.herokuapp.com/api/posts');
+  const json = await raw.json();
+
+  stateSetter(JSON.stringify(json.data.posts));
+};
+
 function App() {
   const [posts, setPosts] = useState('');
 
-  const buildPosts = (posts) => {
-    return JSON.parse(posts).map((post) => {
-      const { body } = post;
-      return (
-        <Post
-          id={post._id}
-          title={post.title}
-          body={`${body.length > 200 ? body.slice(0, 200) : body}...`}
-          author={post.author}
-          stamp={post.timestamp}
-        />
-      );
-    });
-  };
-
-  const getPosts = async () => {
-    const raw = await fetch('https://odingblogapi.herokuapp.com/api/posts');
-    const json = await raw.json();
-
-    setPosts(JSON.stringify(json.data.posts));
-  };
-
-  getPosts();
+  //Run async function that will call setPosts with the retrieved data
+  getPosts(setPosts);
 
   return (
     <div className="container mx-auto max-w-3xl">
