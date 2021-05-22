5b55be43457ed2dfe7c3a00582687f28ddcb6955
ABojo <alex.bojorquez2598@gmail.com>
2021-05-22 17:05:51 -0400

    Limited the body text that is shown to 200 characters as its meant to be a preview

--- src/App.js
+++ src/App.js
@@ -7,21 +7,24 @@ function App() {
   const [posts, setPosts] = useState('');
 
   const buildPosts = (posts) => {
-    return JSON.parse(posts).map((post) => (
-      <Post
-        title={post.title}
-        body={post.body}
-        author={post.author}
-        stamp={post.timestamp}
-      />
-    ));
+    return JSON.parse(posts).map((post) => {
+      const { body } = post;
+      return (
+        <Post
+          title={post.title}
+          body={`${body.length > 200 ? body.slice(0, 200) : body}...`}
+          author={post.author}
+          stamp={post.timestamp}
+        />
+      );
+    });
   };
 
   const getPosts = async () => {
     const raw = await fetch('https://odingblogapi.herokuapp.com/api/posts');
     const json = await raw.json();
 
-    //setPosts(JSON.stringify(json.data.posts));
+    setPosts(JSON.stringify(json.data.posts));
   };
 
   getPosts();
