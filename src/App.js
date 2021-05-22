a9614e604cf5733b3d20b92610f3f486796f99a4
ABojo <alex.bojorquez2598@gmail.com>
2021-05-22 13:49:49 -0400

    Created navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { useState } from 'react';
+import Navbar from './Navbar';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container mx-auto max-w-3xl">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,38 @@
+function Navbar() {
+  return (
+    <div className="flex flex-row items-baseline justify-between px-5 py-5 border-b border-grey-100 mb-10">
+      <a href="/" class="text-3xl font-bold text-blue-500">
+        <i class="fas fa-blog text-blue-500 bg-blue-100 p-4 rounded-full"></i>
+      </a>
+
+      <div class="relative">
+        <input
+          type="text"
+          placeholder="Search posts"
+          class="bg-gray-100 rounded-full pl-8 pr-28 py-4 shadow-inner w-64"
+        />
+        <button
+          class="
+              none
+              search
+              absolute
+              right-1
+              rounded-full
+              px-5
+              py-3
+              bg-blue-500
+              hover:bg-blue-600
+              text-white text-sm
+              font-bold
+              transition
+              duration-200
+            "
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
