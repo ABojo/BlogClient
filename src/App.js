eaac8bcc15f6b12e3446e6b6c1e01d09952879e4
ABojo <alex.bojorquez2598@gmail.com>
2021-05-22 20:03:12 -0400

    Created a post list component to house all of the functionality related to retrieving and building posts so once I introduce a react router the code of the main app component is cleaner

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
 import { useState } from 'react';
 import Navbar from './Navbar';
-import Post from './Post';
-import Loader from './Loader';
-
-const buildPosts = (posts) => {
-  return JSON.parse(posts).map((post) => {
-    const { body } = post;
-    const ts = new Date(post.timestamp);
-    return (
-      <Post
-        url={`/posts/${post._id}`}
-        title={post.title}
-        body={`${body.length > 200 ? body.slice(0, 200) : body}...`}
-        author={post.author}
-        stamp={`${ts.getMonth() + 1}/${ts.getDate()}`}
-      />
-    );
-  });
-};
-
-const getPosts = async (stateSetter) => {
-  const raw = await fetch('https://odingblogapi.herokuapp.com/api/posts');
-  const json = await raw.json();
-
-  stateSetter(JSON.stringify(json.data.posts));
-};
+import PostList from './PostList';
 
 function App() {
-  const [posts, setPosts] = useState('');
-
-  //Run async function that will call setPosts with the retrieved data
-  getPosts(setPosts);
-
   return (
     <div className="container mx-auto max-w-3xl">
       <Navbar />
-      <div>{posts ? buildPosts(posts) : <Loader />}</div>
+      <PostList />
     </div>
   );
 }
---
+++ src/PostList.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import Post from './Post';
+import Loader from './Loader';
+
+const buildPosts = (posts) => {
+  return JSON.parse(posts).map((post) => {
+    const { body } = post;
+    const ts = new Date(post.timestamp);
+    return (
+      <Post
+        url={`/posts/${post._id}`}
+        title={post.title}
+        body={`${body.length > 200 ? body.slice(0, 200) : body}...`}
+        author={post.author}
+        stamp={`${ts.getMonth() + 1}/${ts.getDate()}`}
+      />
+    );
+  });
+};
+
+const getPosts = async (stateSetter) => {
+  const raw = await fetch('https://odingblogapi.herokuapp.com/api/posts');
+  const json = await raw.json();
+
+  stateSetter(JSON.stringify(json.data.posts));
+};
+
+function PostList() {
+  const [posts, setPosts] = useState('');
+
+  //Run async function that will call setPosts with the retrieved data
+  getPosts(setPosts);
+
+  return <div>{posts ? buildPosts(posts) : <Loader />}</div>;
+}
+
+export default PostList;
