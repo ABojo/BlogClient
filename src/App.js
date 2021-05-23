a74a4c092d3583e444cf8c70de13514f23c3d24a
ABojo <alex.bojorquez2598@gmail.com>
2021-05-22 21:20:52 -0400

    Created a full post component for the singular post pages + added a router to change pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { useState } from 'react';
 import Navbar from './Navbar';
 import PostList from './PostList';
+import FullPost from './FullPost';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="container mx-auto max-w-3xl">
       <Navbar />
-      <PostList />
+      <Router>
+        <Switch>
+          <Route exact path="/" children={<PostList />} />
+          <Route exact path="/posts/:id" children={<FullPost />} />
+        </Switch>
+      </Router>
     </div>
   );
 }
