a488ee70607036bfccafd4c18736fadc9c5738ec
ABojo <alex.bojorquez2598@gmail.com>
2021-05-23 20:21:55 -0400

    Added markup and logic for rendering comments to the page

--- src/FullPost.js
+++ src/FullPost.js
@@ -23,29 +23,48 @@ const buildPost = (post) => {
   );
 };
 
-const buildComments = () => {
+const buildComments = (post) => {
+  const { comments } = JSON.parse(post);
+  console.log(comments);
+
   return (
-    <div className="bg-gray-100 p-5">
-      <form className="mx-auto">
-        <input
-          type="text"
-          placeholder="Enter your name"
-          className="px-5 py-5 rounded shadow-inner resize-none w-full text-sm block mb-3"
-          required
-        />
-        <textarea
-          placeholder="Enter a comment"
-          className="px-5 py-5 rounded shadow-inner resize-none w-full text-sm block mb-3"
-          required
-        />
-        <input
-          onClick={(e) => e.preventDefault()}
-          type="submit"
-          value="Add Comment"
-          className="rounded font-bold bg-blue-100 hover:bg-blue-200 text-blue-900 p-3 text-sm cursor-pointer transition duration-200 shadow block ml-auto"
-        />
-      </form>
-    </div>
+    <React.Fragment>
+      <div className="bg-gray-100 p-5 mb-10 rounded">
+        <form className="mx-auto">
+          <input
+            type="text"
+            placeholder="Enter your name"
+            className="px-5 py-5 rounded shadow-inner resize-none w-full text-sm block mb-3"
+            required
+          />
+          <textarea
+            placeholder="Enter a comment"
+            className="px-5 py-5 rounded shadow-inner resize-none w-full text-sm block mb-3"
+            required
+          />
+          <input
+            onClick={(e) => e.preventDefault()}
+            type="submit"
+            value="Add Comment"
+            className="rounded font-bold bg-blue-100 hover:bg-blue-200 text-blue-900 p-3 text-sm cursor-pointer transition duration-200 shadow block ml-auto"
+          />
+        </form>
+      </div>
+      <div class="mb-10">
+        <h1 class="text-3xl mb-3 text-gray-900">
+          Comments ({comments.length})
+        </h1>
+        {comments.reverse().map((com) => {
+          return (
+            <div className="border-l-8 border-blue-500 px-8 mb-3">
+              <h1 class="text-xl text-gray-900">{com.name}</h1>
+              <h1 class="text-gray-400 text-lg mb-3">{com.body}</h1>
+              <h1 class="text-gray-300">{com.timestamp}</h1>
+            </div>
+          );
+        })}
+      </div>
+    </React.Fragment>
   );
 };
 
@@ -53,7 +72,7 @@ const buildPostAndComments = (post) => {
   return (
     <React.Fragment>
       {buildPost(post)}
-      {buildComments()}
+      {buildComments(post)}
     </React.Fragment>
   );
 };
