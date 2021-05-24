01cff858dda97a5389e50cb2d211ffbbff6f21d3
ABojo <alex.bojorquez2598@gmail.com>
2021-05-23 20:32:02 -0400

    Replaced search bar with a link to the admin panel

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,32 +5,24 @@ function Navbar() {
         <i className="fas fa-blog text-blue-500 bg-blue-100 p-4 rounded-full"></i>
       </a>
 
-      <div className="relative">
-        <input
-          type="text"
-          placeholder="Search posts"
-          className="bg-gray-100 rounded-full pl-8 pr-28 py-4 shadow-inner w-64"
-        />
-        <button
-          className="
-              none
-              search
-              absolute
-              right-1
-              rounded-full
-              px-5
-              py-3
-              bg-blue-500
-              hover:bg-blue-600
-              text-white text-sm
-              font-bold
-              transition
-              duration-200
-            "
-        >
-          Search
-        </button>
-      </div>
+      <a
+        href="/admin"
+        className="
+            right-1
+            rounded
+            px-5
+            py-3
+            bg-blue-500
+            hover:bg-blue-600
+            text-white text-sm
+            font-bold
+            transition
+            duration-200
+            text-lg
+          "
+      >
+        Admin
+      </a>
     </div>
   );
 }
