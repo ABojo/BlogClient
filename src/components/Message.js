dfbee12282ae01ba2c67bd82a60e418a138df609
ABojo <alex.bojorquez2598@gmail.com>
2021-05-25 15:15:24 -0400

    I was using concatenation to build the class names for the error message which resulted in the classes being purged from my production build. The tailwind documentation recommends using the entire class name to avoid it being purged

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,12 @@
 function Message(props) {
   const { text, success } = props;
   const color = success ? 'green' : 'red';
+  const backgroundColor = success ? 'bg-green-100' : 'bg-red-100';
+  const textColor = success ? 'text-green-900' : 'text-red-900';
 
   return (
-    <div className={`p-5 w-full bg-${color}-100 rounded mb-10`}>
-      <h1 className={`text-${color}-900`}>{text}</h1>
+    <div className={`p-5 w-full ${backgroundColor} rounded mb-10`}>
+      <h1 className={textColor}>{text}</h1>
     </div>
   );
 }
