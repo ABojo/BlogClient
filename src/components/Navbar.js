8c6e1c98711618304e7560bd2f17bd588e8b0617
ABojo <alex.bojorquez2598@gmail.com>
2021-05-24 20:05:14 -0400

    Updated navbar component by adding link to repository + removed the blog icon for a text heading

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,14 @@
 function Navbar() {
   return (
     <div className="flex flex-row items-baseline justify-between px-5 py-5 border-b border-grey-100 mb-10">
-      <a href="/" className="text-3xl font-bold text-blue-500">
-        <i className="fas fa-blog text-blue-500 bg-blue-100 p-4 rounded-full"></i>
+      <a href="/" className="text-3xl">
+        Blog Client
       </a>
-
       <a
-        href="/admin"
-        className="
-            right-1
-            rounded
-            px-5
-            py-3
-            bg-blue-500
-            hover:bg-blue-600
-            text-white text-sm
-            font-bold
-            transition
-            duration-200
-            text-lg
-          "
+        href="https://github.com/ABojo/BlogClient"
+        className="text-3xl font-bold text-blue-500"
       >
-        Admin
+        <i className="fab fa-github text-blue-500 bg-blue-100 p-4 rounded-full"></i>
       </a>
     </div>
   );
